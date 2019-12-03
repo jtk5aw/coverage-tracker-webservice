f790ff750c1041862cca6636dd91af0d63d92a0e
Jackson Kennedy <jtk5aw@virginia.edu>
2019-12-03 01:15:04 -0500

    Fix migrations

--- coverage_tracker/migrations/0002_auto_20191202_2358.py
+++ coverage_tracker/migrations/0002_auto_20191202_2358.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='staffer',
             name='phone_number',
-            field=phonenumber_field.modelfields.PhoneNumberField(default='5714395221', max_length=128, region=None),
+            field=phonenumber_field.modelfields.PhoneNumberField(default='+5714395221', max_length=128, region=None),
         ),
     ]
