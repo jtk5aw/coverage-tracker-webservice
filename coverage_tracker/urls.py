b5816a3b8dc68e526f20256c228b3c3a06841f3d
Jackson Kennedy <jtk5aw@virginia.edu>
2019-12-03 01:10:26 -0500

    Set up basic queries for Staffers

--- coverage_tracker/urls.py
+++ coverage_tracker/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from coverage_tracker import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('all/', views.staffer_list),
+    path('by_comp_id/<str:comp_id>/', views.staffer_by_comp_id),
+    path('by_dorm/<str:dorm>/', views.staffer_by_dorm)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- coverage_tracker/views.py
+++ coverage_tracker/views.py
@@ -1,8 +1,69 @@
 from django.contrib.auth.models import User, Group
+
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 
 from .models import Staffer
 from .serializers import UserSerializer, GroupSerializer, StafferSerializer
+
+
+@api_view(['GET', 'POST'])
+def staffer_list(request, format=None):
+    """
+    List all staffers, or create a new snippet.
+    """
+    if request.method == 'GET':
+        staffers = Staffer.objects.all()
+        print(staffers)
+        serializer = StafferSerializer(staffers, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = StafferSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def staffer_by_comp_id(request, comp_id, format=None):
+    """
+    Retrieve a staffer based on comp_id
+    """
+    try:
+        staffer = Staffer.objects.get(comp_id=comp_id)
+    except Staffer.DoesNotExist:
+        return Response([])
+
+    if request.method == 'GET':
+        serializer = StafferSerializer(staffer)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = StafferSerializer(staffer, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        staffer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def staffer_by_dorm(request, dorm, format=None):
+    """
+    Retrieve a list of staffers given a dorm name
+    """
+    staffers = Staffer.objects.filter(building=dorm)
+
+
+    if request.method == 'GET':
+        serializer = StafferSerializer(staffers, many=True)
+        return Response(serializer.data)
+
 
 class StafferViewSet(viewsets.ModelViewSet):
     """
